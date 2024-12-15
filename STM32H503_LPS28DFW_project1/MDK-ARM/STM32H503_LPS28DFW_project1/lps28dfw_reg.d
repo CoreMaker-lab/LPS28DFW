f2d3dd081de928e30934013579a2163e3b275765
CoreMaker <a845656974@outlook.com>
2024-12-15 23:08:24 +0800

    update

---
+++ STM32H503_LPS28DFW_project1/MDK-ARM/STM32H503_LPS28DFW_project1/lps28dfw_reg.d
@@ -0,0 +1,5 @@
+stm32h503_lps28dfw_project1/lps28dfw_reg.o: ..\Core\Src\lps28dfw_reg.c \
+  ..\Core\Inc\lps28dfw_reg.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\math.h
---
+++ STM32H503_LPS28DFW_project2/Core/Src/main.c
@@ -0,0 +1,329 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "i2c.h"
+#include "icache.h"
+#include "memorymap.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "stdio.h"
+
+#include "lps28dfw_reg.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+int fputc(int ch, FILE *f){
+	HAL_UART_Transmit(&huart1 , (uint8_t *)&ch, 1, 0xFFFF);
+	return ch;
+}
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+#define SENSOR_BUS hi2c1
+
+/* Private macro -------------------------------------------------------------*/
+#define    BOOT_TIME         10 //ms
+
+/* Private variables ---------------------------------------------------------*/
+static uint8_t tx_buffer[1000];
+static lps28dfw_data_t data;
+
+/* Extern variables ----------------------------------------------------------*/
+
+/* Private functions ---------------------------------------------------------*/
+/*
+ *   WARNING:
+ *   Functions declare in this section are defined at the end of this file
+ *   and are strictly related to the hardware platform used.
+ *
+ */
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
+                              uint16_t len);
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                             uint16_t len);
+static void tx_com( uint8_t *tx_buffer, uint16_t len );
+static void platform_delay(uint32_t ms);
+static void platform_init(void);
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_I2C1_Init();
+  MX_USART1_UART_Init();
+  MX_ICACHE_Init();
+  /* USER CODE BEGIN 2 */
+	printf("1111111111111111\n");
+
+  lps28dfw_pin_int_route_t int_route;
+  lps28dfw_all_sources_t all_sources;
+  lps28dfw_bus_mode_t bus_mode;
+  lps28dfw_stat_t status;
+  stmdev_ctx_t dev_ctx;
+  lps28dfw_id_t id;
+  lps28dfw_md_t md;
+
+  /* Initialize mems driver interface */
+  dev_ctx.write_reg = platform_write;
+  dev_ctx.read_reg = platform_read;
+  dev_ctx.mdelay = platform_delay;
+  dev_ctx.handle = &SENSOR_BUS;
+
+
+	  HAL_GPIO_WritePin(SA0_GPIO_Port, SA0_Pin, GPIO_PIN_RESET);
+
+  /* Initialize platform specific hardware */
+//  platform_init();
+
+  /* Wait sensor boot time */
+  platform_delay(BOOT_TIME);
+
+  /* Check device ID */
+  lps28dfw_id_get(&dev_ctx, &id);
+	printf("LPS28DFW_ID=0x%x,id.whoami=0x%x\n",LPS28DFW_ID,id.whoami);	
+  if (id.whoami != LPS28DFW_ID)
+    while(1);
+
+  /* Restore default configuration */
+  lps28dfw_init_set(&dev_ctx, LPS28DFW_RESET);
+  do {
+    lps28dfw_status_get(&dev_ctx, &status);
+  } while (status.sw_reset);
+
+  /* Set bdu and if_inc recommended for driver usage */
+  lps28dfw_init_set(&dev_ctx, LPS28DFW_DRV_RDY);
+
+  /* Select bus interface */
+  bus_mode.filter = LPS28DFW_AUTO;
+  bus_mode.interface = LPS28DFW_SEL_BY_HW;
+  lps28dfw_bus_mode_set(&dev_ctx, &bus_mode);
+
+  /* Set Output Data Rate */
+  md.odr = LPS28DFW_1Hz;
+  md.avg = LPS28DFW_512_AVG;
+  md.lpf = LPS28DFW_LPF_ODR_DIV_9;
+  md.fs = LPS28DFW_1260hPa;
+  lps28dfw_mode_set(&dev_ctx, &md);
+
+  /* Configure inerrupt pins */
+  lps28dfw_pin_int_route_get(&dev_ctx, &int_route);
+  int_route.drdy_pres   = PROPERTY_DISABLE;
+  lps28dfw_pin_int_route_set(&dev_ctx, &int_route);
+	int i=0;
+	float open_h;
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+		
+  /* Read output only if new values are available */
+    lps28dfw_all_sources_get(&dev_ctx, &all_sources);
+    if ( all_sources.drdy_pres | all_sources.drdy_temp ) {
+      lps28dfw_data_get(&dev_ctx, &md, &data);
+			if(i==0)
+			{
+				open_h	=data.pressure.hpa;
+				printf("初始化压强:%6.2fhPa,温度:%6.2f\r\n",open_h,data.heat.deg_c);
+				i++;
+			}
+			else
+			{
+				float h;
+				h=(data.pressure.hpa-open_h)/(1000*9.8);//水深=压强/(水密度*重力加速度)
+				//1百帕 (hPa)= 100 Pa = 1 mbar1千帕 (kPa)= 1000 Pa = 10 hPa
+				printf("压强:%6.2fhPa,水深:%6.2f cm,温度:%6.2f\r\n",data.pressure.hpa,h*100*100,data.heat.deg_c);
+			}
+    }	
+		
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0);
+
+  while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_CSI;
+  RCC_OscInitStruct.CSIState = RCC_CSI_ON;
+  RCC_OscInitStruct.CSICalibrationValue = RCC_CSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLL1_SOURCE_CSI;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 125;
+  RCC_OscInitStruct.PLL.PLLP = 2;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1_VCIRANGE_2;
+  RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1_VCORANGE_WIDE;
+  RCC_OscInitStruct.PLL.PLLFRACN = 0;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
+                              |RCC_CLOCKTYPE_PCLK3;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB3CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure the programming delay
+  */
+  __HAL_FLASH_SET_PROGRAM_DELAY(FLASH_PROGRAMMING_DELAY_2);
+}
+
+/* USER CODE BEGIN 4 */
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
+                              uint16_t len)
+{
+  HAL_I2C_Mem_Write(handle, LPS28DFW_I2C_ADD_L, reg,
+                    I2C_MEMADD_SIZE_8BIT, (uint8_t*) bufp, len, 1000);
+  return 0;
+}
+
+
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                             uint16_t len)
+{
+  HAL_I2C_Mem_Read(handle, LPS28DFW_I2C_ADD_L, reg,
+                   I2C_MEMADD_SIZE_8BIT, bufp, len, 1000);
+  return 0;
+}
+
+static void platform_delay(uint32_t ms)
+{
+  HAL_Delay(ms);
+}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
